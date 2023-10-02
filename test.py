b442d63e8e00d8f0f0895d6979fcb697e1277afc
cxzaqq <roy1228@naver.com>
2023-10-02 21:30:49 +0900

    test

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
-from profanity_check import predict
+from profanity_check import predict, predict_prob
 
-text = ["Hello, this is a clean text.", "fuck you!"]
+text = ["Hello, this is a clean text.", "fukc you!"]
 
-results = predict(text)
+results = predict_prob(text)
 
 for i, result in enumerate(results):
-    if result == 1:
-        print(f"Text {i+1}: 욕설이 포함되어 있습니다.")
-    else:
-        print(f"Text {i+1}: 욕설이 없습니다.")
+    print(results)
+
+
+# for i, result in enumerate(results):
+#     if result == 1:
+#         print(f"Text {i+1}: 욕설이 포함되어 있습니다.")
+#     else:
+#         print(f"Text {i+1}: 욕설이 없습니다.")
